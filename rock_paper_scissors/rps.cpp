f38ae73ab63f7cf1f64b4f94b0a78af4a46f02ea
Aaron Evans <aarone0621@gmail.com>
2026-01-25 22:49:25 -0600

    rock paper scissors

---
+++ rock_paper_scissors/rps.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string temp1, temp2;
+    while (getline(cin, temp1) && getline(cin, temp2)) {
+        pair<int, int> scores = {0, 0};  // p1, p2
+        if (temp1 == "E" && temp2 == "E") break;
+
+        // probably a better way than nested but small data set
+
+        for (int i = 0; i < temp1.length(); i++) {
+            if (temp1[i] == temp2[i]) continue;
+
+            if (temp1[i] == 'R') {
+                if (temp2[i] == 'P')
+                    scores.second += 1;
+                else
+                    scores.first += 1;
+            }
+            else if (temp1[i] == 'P') {
+                if (temp2[i] == 'S')
+                    scores.second += 1;
+                else
+                    scores.first += 1;
+            }
+            else {  // p1 is S
+                if (temp2[i] == 'R')
+                    scores.second += 1;
+                else
+                    scores.first += 1;
+            }
+        }
+
+        cout << "P1: " << scores.first << endl;
+        cout << "P2: " << scores.second << endl;
+    }
+
+    return 0;
+}
